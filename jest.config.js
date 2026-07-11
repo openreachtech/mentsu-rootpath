fa7a172bcfa26bf0063e79e85463fcecaad03338
Stew Eucen <stew.eucen@gmail.com>
2026-07-11 11:24:52 +0900

    Update setting code from setupFilesAfterEnv in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 export default {
   setupFilesAfterEnv: [
-    '<rootDir>/node_modules/@openreachtech/renchan-test-tools/lib/environment/setupAfterEnv.js',
     '<rootDir>/tests/setup-after-env.js',
   ],
   moduleNameMapper: {
