cd7d8d9e9acecf38b2000bab16e1aab6a26b36a9
Stew Eucen <12193670+StewEucen@users.noreply.github.com>
2026-09-25 13:29:50 +0900

    Kick out the temporary relaxations from eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,4 @@ import openreachtechConfig from '@openreachtech/eslint-config'
 
 export default [
   ...openreachtechConfig,
-
-  // Turn off some rules temporary
-  {
-    rules: {
-      camelcase: 'off',
-      'sort-imports': 'off',
-
-      'jest/max-expects': 'off',
-      'jest/max-nested-describe': 'off',
-      // TODO: Update @openreachtech/eslint-rules-default-jest with the latest version of eslint-plugin-jest
-      'jest/no-deprecated-functions': 'off',
-
-      'jsdoc/check-indentation': 'off',
-      'jsdoc/check-tag-names': 'off',
-      'jsdoc/no-undefined-types': 'off',
-      'jsdoc/require-jsdoc': 'off',
-      'jsdoc/valid-types': 'off',
-    },
-  },
 ]
